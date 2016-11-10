455fe70bc2184e6ea07da5e048f778a69e93a970
zhengbangc <zhengbangc@gmail.com>
2016-11-09 20:06:43 -0600

    read input successful

--- mp3.cpp
+++ mp3.cpp
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <chrono> // C++11
+#include <unordered_map>
 using namespace std;
 
 /** 
@@ -21,20 +22,59 @@ using namespace std;
 * for game of breakthrough
 */
 int main(int argc, char** args) {
-	
 
-	if (args[1][0]=='c')
-	{
-
-	}
-
-	if (argc != 4 && args[1][0] == 's') {
-			cout << "Wrong input format. Expected ./mp2 [s] [gridfile] [wordbank]"<<endl;
-			return 1;
-	}
-
+	//part 2
 	if (args[1][0] == 't') {
+		if (argc != 5) {
+			cout << "Wrong input format! Expected: ./mp3 [t] [model] [trainfile] [testfile]"<<endl;
+			return 1;
+		}
 
+		//multinomial model
+		if (args[2][0] == 'm') { 
+			ifstream trainfile(args[3]);
+			ifstream testfile(args[4]);
+			int k = 0; //number of class labels
+			int n = 0; //the length of a single document
+			int m = 0; //the size of the vocabulary
+			string line;
+			vector<int> labels;
+			unordered_map<string, float> unique_strings;
+			while (getline(trainfile, line)) {
+				string curWord;
+				string curCount;
+				int curCountInt;
+				int i;
+				if (line[0] == '-') { //if label is -1
+					labels.push_back(-1);
+					i = 2;
+				}
+				else { //label is 1
+					labels.push_back(1);
+					i = 1;					
+				}
+				while (i < line.length()) {
+					if (line[i] >= '0' && line[i] <= '9') {
+						curCount.push_back(line[i]);
+					} else if (line[i] != ':' && line[i] != ' ') {
+						curWord.push_back(line[i]);
+					} else if (line[i] == ' ')  {
+						if (curWord.size() != 0) {
+							// cout << "curWord is " << curWord << endl;
+							curCountInt = std::stoi(curCount, nullptr, 10);
+							if (unique_strings.find(curWord) == unique_strings.end()) {
+								unique_strings.insert(std::make_pair(curWord, (float)curCountInt));
+							} else {
+								unique_strings[curWord] += (float)curCountInt;
+							}
+							curWord.clear();
+							curCount.clear();	
+						}
+					}
+					i++;
+				}
+			}
+		}
 	} 
 	return 0;
 }
